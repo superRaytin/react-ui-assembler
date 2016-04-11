e9de5ae6516cbda0405ff83d66063528b04a7f85
leon.shi <superRaytin@163.com>
2016-04-11 22:08:01 +0800

    Sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch';
 import { takeEvery, takeLatest } from 'redux-saga';
 import { take, call, fork, put, cancel } from 'redux-saga/effects';
 
@@ -7,26 +8,51 @@ function delay(ms) {
   });
 }
 
-function* fetchxxx(getState) {
-  // add `while (true)` avoid errors when using with generator function
-  // looks like it's babel's problem
+// 获取数据
+async function fetchData(url) {
+  const result = await fetch(url, {
+    type: 'json'
+  }).then(response => response.json());
+
+  return {
+    result
+  };
+}
+
+// 获取所有控件
+function* fetchWidgets(getState) {
+  yield put({
+    type: 'widget/sync/start'
+  });
+
+  const widgets = yield call(fetchData, '/widget/group/getAll.json');
+
+  yield put({
+    type: 'widget/group/sync/all',
+    payload: widgets.result.data,
+  });
+
+  yield put({
+    type: 'widget/backup',
+    payload: widgets.result.data,
+  });
+
+  yield put({
+    type: 'widget/sync/end'
+  });
+}
+
+// ******************************************************************************/
+// ******************************* WATCHERS *************************************/
+// ******************************************************************************/
+
+function* watchFetchWidgets(getState) {
   while (true) {
-    const actionxxx = yield take('abc/xxx');
-    console.log('saga', actionxxx);
-    yield put({
-      type: 'abc/sync/start'
-    });
-    yield call(delay, 1000);
-    yield put({
-      type: 'abc/setCounter',
-      payload: 888,
-    });
-    yield put({
-      type: 'abc/sync/end'
-    });
+    yield take('widget/group/sync');
+    yield fork(fetchWidgets, getState);
   }
 }
 
 export default function* root(getState) {
-  yield fork(fetchxxx, getState);
+  yield fork(watchFetchWidgets, getState);
 }
