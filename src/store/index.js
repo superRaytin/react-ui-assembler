f6e11e4b26d81f54d5948c59ab061a590e5fea7f
leon.shi <superRaytin@163.com>
2016-04-11 22:08:42 +0800

    Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -12,5 +12,12 @@ export default function configureStore(initialState) {
 
   const store = createStore(reducers, initialState, enhancer);
 
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer);
+    });
+  }
+
   return store;
 }
