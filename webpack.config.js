f9c3e944fd105341b6e897ce6b739a790aacf135
leon.shi <superRaytin@163.com>
2016-04-11 22:10:16 +0800

    Update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,5 @@ module.exports = function(webpackConfig) {
     loader: 'es3ify-loader',
   });
 
-  webpackConfig.module.loaders.forEach(function(item) {
-    if (item.loader.indexOf('css?sourceMap')) {
-      item.loader = item.loader.replace('css?sourceMap',
-        'css?modules&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]');
-    }
-  });
-
   return webpackConfig;
 };
