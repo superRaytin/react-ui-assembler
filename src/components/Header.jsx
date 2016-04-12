32ee6f20589c06442931a589b30b11455a690fdd
leon.shi <superRaytin@163.com>
2016-04-12 23:08:45 +0800

    New header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import React, { PropTypes, Component } from 'react';
+import {
+  Icon
+} from 'antd';
 
 class Header extends Component {
   render() {
     return (
-      <header className="silo-header">X pro</header>
+      <header className="silo-header">
+        <div className="silo-header-logo">
+          X pro
+        </div>
+        <div className="silo-header-nav">
+          <ul>
+            <li className="active">
+              <Icon type="desktop" />
+              <span>工作台</span>
+            </li>
+            <li>
+              <Icon type="file-text" />
+              <span>模板</span>
+            </li>
+            <li>
+              <Icon type="user" />
+              <span>我的</span>
+            </li>
+          </ul>
+        </div>
+      </header>
     );
   }
 }
