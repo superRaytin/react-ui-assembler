b7d0e61a5633379e287d0614f4aaa49ec5518c66
leon.shi <superRaytin@163.com>
2016-04-17 16:14:32 +0800

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,63 @@
 import React, { PropTypes, Component } from 'react';
+import { Link } from 'react-router';
 import {
   Icon
 } from 'antd';
 
 class Header extends Component {
+  generateNavList() {
+    const navList = [
+      {
+        name: '工作台',
+        icon: 'desktop',
+        path: '/workspace'
+      },
+      {
+        name: '我的',
+        icon: 'user',
+        path: '/personal'
+      },
+      {
+        name: '开发者',
+        icon: 'github',
+        path: '/developer'
+      },
+    ];
+
+    const currentRouteName = this.props.route.path;
+
+    return navList.map((nav, i) => {
+      return (
+        <li key={i} className={`/${currentRouteName}` === nav.path ? 'active' : ''}>
+          <Link to={nav.path}>
+            <Icon type={nav.icon} />
+            <span>{nav.name}</span>
+          </Link>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <header className="silo-header">
         <div className="silo-header-logo">
-          X pro
+          <Icon type="appstore" />
+          <div>Ant Pro</div>
         </div>
         <div className="silo-header-nav">
           <ul>
-            <li className="active">
-              <Icon type="desktop" />
-              <span>工作台</span>
-            </li>
-            <li>
-              <Icon type="file-text" />
-              <span>模板</span>
-            </li>
-            <li>
-              <Icon type="user" />
-              <span>我的</span>
-            </li>
+            {this.generateNavList()}
           </ul>
         </div>
       </header>
     );
   }
 }
+
+Header.propTypes = {
+  route: PropTypes.object
+};
 
 export default Header;
 
