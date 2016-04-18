ecc231b16e7b92dd1ba352c8e25ac27139d1697a
leon.shi <superRaytin@163.com>
2016-04-18 13:20:06 +0800

    Nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,28 +5,10 @@ import {
 } from 'antd';
 
 class Header extends Component {
-  generateNavList() {
-    const navList = [
-      {
-        name: '工作台',
-        icon: 'desktop',
-        path: '/workspace'
-      },
-      {
-        name: '我的',
-        icon: 'user',
-        path: '/personal'
-      },
-      {
-        name: '开发者',
-        icon: 'github',
-        path: '/developer'
-      },
-    ];
-
+  generateNavList(dataList) {
     const currentRouteName = this.props.route.path;
 
-    return navList.map((nav, i) => {
+    return dataList.map((nav, i) => {
       return (
         <li key={i} className={`/${currentRouteName}` === nav.path ? 'active' : ''}>
           <Link to={nav.path}>
@@ -39,6 +21,32 @@ class Header extends Component {
   }
 
   render() {
+    const topNavList = [
+      {
+        name: '工作台',
+        icon: 'desktop',
+        path: '/workspace'
+      },
+      {
+        name: '我的',
+        icon: 'user',
+        path: '/personal'
+      },
+    ];
+
+    const bottomNavList = [
+      {
+        name: '帮助',
+        icon: 'question-circle-o',
+        path: '/help'
+      },
+      {
+        name: '开发者',
+        icon: 'github',
+        path: '/developer'
+      },
+    ];
+
     return (
       <header className="silo-header">
         <div className="silo-header-logo">
@@ -47,7 +55,11 @@ class Header extends Component {
         </div>
         <div className="silo-header-nav">
           <ul>
-            {this.generateNavList()}
+            {this.generateNavList(topNavList)}
+          </ul>
+          <div className="flex-placeholder">&nbsp;</div>
+          <ul style={{paddingBottom: 20}}>
+            {this.generateNavList(bottomNavList)}
           </ul>
         </div>
       </header>
